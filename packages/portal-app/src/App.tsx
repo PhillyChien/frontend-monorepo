79feace91f362740c8fd6e3f656500a51338ed8e
Philly Chien <chienaeae>
2025-02-04 14:13:21 -0500

    feat(portal-app): add application example components

--- packages/portal-app/src/App.tsx
+++ packages/portal-app/src/App.tsx
@@ -1,53 +1,17 @@
-import { Button, Input } from '@frontend-monorepo/ui';
-import { useEffect, useState } from 'react';
+import { OwnFundApplication, QuickLoanApplication } from '@frontend-monorepo/ui';
 import { useTranslation } from 'react-i18next';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 function App() {
-  const [nums, setNums] = useState<{
-    x: number;
-    y: number;
-  }>({ x: 0, y: 0 });
-
-  const [result, setResult] = useState<number>(0);
-
-  const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    i18n.changeLanguage('en');
-  }, [i18n, i18n.language]);
-
+  const { t } = useTranslation();
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <div className="shadow-4xl flex w-full max-w-md flex-col gap-4 rounded-md p-4">
-        <div className="mb-8 text-2xl font-bold">{t('title')}</div>
-        <Input
-          type="number"
-          placeholder="Enter X"
-          value={nums.x}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setNums({ ...nums, x: parseInt(e.target.value) })
-          }
-        />
-        <Input
-          type="number"
-          placeholder="Enter Y"
-          value={nums.y}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setNums({ ...nums, y: parseInt(e.target.value) })
-          }
-        />
-        <Button
-          size="lg"
-          variant="default"
-          onClick={() => {
-            setResult(nums.x + nums.y);
-          }}
-        >
-          Calculate
-        </Button>
-        <Input disabled type="number" value={result} />
-      </div>
-    </div>
+    <BrowserRouter>
+      <h1>{t('title')}</h1>
+      <Routes>
+        <Route path="application/*" element={<QuickLoanApplication />} />
+        <Route path="own-fund/*" element={<OwnFundApplication />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
