8e8825144171f236a6eb4eca0b9168581fb0c0b5
Philly Chien <chienaeae>
2025-02-05 23:39:36 -0500

    feat(portal-app): initialize backend client

--- packages/portal-app/src/App.tsx
+++ packages/portal-app/src/App.tsx
@@ -1,9 +1,27 @@
+import { BackendClient } from '@frontend-monorepo/backend-client';
 import { OwnFundApplication, QuickLoanApplication } from '@frontend-monorepo/ui';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BrowserRouter, Route, Routes } from 'react-router';
 
+const backendClient = new BackendClient({
+  apiClientOptions: {
+    baseUrl: 'http://localhost:3000',
+
+    refreshTokenThresholdMs: 1000,
+    refreshTokenCallback: () => Promise.resolve(''),
+  },
+});
+
 function App() {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    backendClient.user.getMe().then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <h1>{t('title')}</h1>
